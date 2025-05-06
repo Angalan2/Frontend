04eb6c0ae374f790b60307e6e55f7e828eb15b40
Angalan2 <angalan792002@gmail.com>
2025-05-06 21:04:08 +0530

    constant.js

---
+++ constant.js
@@ -0,0 +1,22 @@
+const name = 1;
+
+const uname = "Angalan2";
+const pwd = "this my pwd";
+
+
+//hardcore value
+
+//colors
+
+const WHITE_COLOR = "#fff";
+const BLACK_COLOR = "#000";
+login(uname, pwd);
+
+
+
+const DATE_OF_BIRTH = "09-09-2002";
+const age = calAge(DATE_OF_BIRTH);
+
+
+
+

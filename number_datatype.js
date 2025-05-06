6b5b4e5c30228ae5af5a4fc640c672135f96e09a
Angalan2 <angalan792002@gmail.com>
2025-05-06 21:06:06 +0530

    number_datatype.js

---
+++ number_datatype.js
@@ -0,0 +1,18 @@
+//number datatype
+let pi = 3.14;
+let prime = 24;
+let nan = NaN;
+//console.log(pi,prime);
+
+nan = NaN +2;
+console.log(typeof prime);
+
+//special nueric values
+
+let infi = Infinity;
+let negInfi = -Infinity;
+
+console.log(infi);
+console.log(negInfi);
+
+console.log(typeof infi);

0db096220b3aff76014104ade4c014cdfab5ce60
Angalan2 <angalan792002@gmail.com>
2025-05-06 21:08:44 +0530

    String.js

---
+++ String.js
@@ -0,0 +1,19 @@
+//string datatype
+
+let intro = "hi is this angalan";
+
+let channel = "this is gom's channel";
+console.log(intro);
+
+
+//back tick
+
+let para = `"this Batman's homes".`
+console.log(para);
+
+
+
+//emebeding used in back tick
+
+let embed =`1+2 = $(1+2)`;
+console.log(embed);

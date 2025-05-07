5ef58808dd57400d4279b82251d1b3f039787fef
Angalan2 <angalan792002@gmail.com>
2025-05-07 21:05:09 +0530

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,19 @@
+let person;
+let age = 10;
+if (age > 18){
+    person = 'adult';
+}
+else if (age > 12){
+    person = 'teen';
+}
+else{
+    person = 'child';
+}
+console.log(person);
+
+//conditonal operator
+
+let y;
+let z = 20;
+y = (z>18) ? 'adult' : 'child';
+console.log(y);

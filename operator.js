47d1ebd81c341d702174a21d05fff4faa6f56e09
Angalan2 <angalan792002@gmail.com>
2025-05-07 21:03:24 +0530

    operator.js

---
+++ operator.js
@@ -0,0 +1,29 @@
+//unary
+
+let x = 1;
+x = -x;
+console.log(x);
+
+//binary
+
+let y = 12, z=12;
+let sum = y+z;
+console.log(sum);
+
+//power
+
+console.log(x**z);
+
+//modify in place
+
+let a = 12;
+a += 5;
+console.log(a);
+
+//incremment  decreement
+
+
+console.log(++a);
+console.log(--a);
+console.log(a++);
+console.log(a);

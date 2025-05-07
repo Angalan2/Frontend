594d04fcf1a8569dcbcf9cc920fe9f1f51587873
Angalan2 <angalan792002@gmail.com>
2025-05-07 21:00:46 +0530

    typeconversion.js

---
+++ typeconversion.js
@@ -0,0 +1,24 @@
+console.log('12'/'2');
+
+//type casting
+
+let age = 23;
+console.log(typeof age);
+age = String (age);
+console.log(age);
+
+//number datatype conversion
+
+let y ='23';
+console.log(typeof y);
+y = Number (y);
+console.log(y + 2);
+
+
+//boolean type conversion
+
+let x = 1;
+console.log(typeof x);
+x = Boolean(x);
+console.log(typeof x);
+console.log(x);

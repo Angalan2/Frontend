b4845302a2b47e129869c9214214a0c45daa04a9
Angalan2 <angalan792002@gmail.com>
2025-05-08 21:17:17 +0530

    Switch.js

---
+++ Switch.js
@@ -0,0 +1,16 @@
+let n = 2;
+switch (n){
+    case 1: 
+       console.log('one');
+     break;
+    case 2:
+        console.log('two');
+        break;
+    case 3:
+        console.log('three');
+        break;
+    default:
+        console.log('not greater');
+    
+
+}
